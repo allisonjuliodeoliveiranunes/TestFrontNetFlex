79ba90be678b0e33111be76652fe3b10dc15e7e2
Allison Nunes <allison.julio@hotmail.com>
2020-04-20 07:39:19 -0300

    Refactoring to html

--- assets/js/min.js
+++ assets/js/min.js
@@ -1,8 +1,8 @@
 $(function () {
-  $("#header").load("pages/header.php");
-  $("#footer").load("pages/footer.php");
-  $("#colapse").load("pages/colapse.php");
-  $("#Cmobile").load("pages/carroussel-mobile.php");
+  $("#header").load("pages/header.html");
+  $("#footer").load("pages/footer.html");
+  $("#colapse").load("pages/colapse.html");
+  $("#Cmobile").load("pages/carroussel-mobile.html");
 });  
 
    
